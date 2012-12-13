485bda2b5266b9ba85cdecd8a1c30061b1ee6997
Petr Lautrbach <plautrba@redhat.com>
2012-12-13 13:02:24 +0100

    revert the 591b81e3 commit

--- utils/oscap-xccdf.c
+++ utils/oscap-xccdf.c
@@ -814,7 +814,9 @@ int app_evaluate_xccdf(const struct oscap_action *action)
 			xccdf_result_set_profile(ritem, id);
 	}
 
-	xccdf_result_fill_sysinfo(ritem);
+	/* Use sysinfo from the first is the historical update frequenct oval session, if exist */
+	if (sessions && sessions[0])
+		oval_agent_export_sysinfo_to_xccdf_result(sessions[0], ritem);
 
 	struct xccdf_model_iterator *model_it = xccdf_benchmark_get_models(benchmark);
 	while (xccdf_model_iterator_has_more(model_it)) {
